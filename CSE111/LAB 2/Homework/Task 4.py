bfd0176d6aacba323013a27efceeb40f48ed88eb
Afnan Mohammad Hafiz <afnan1075@gmail.com>
2025-10-06 18:36:19 +0600

    Update Task 4.py

--- CSE111/LAB 2/Homework/Task 4.py
+++ CSE111/LAB 2/Homework/Task 4.py
@@ -1,23 +1,20 @@
 def username_generator(first, last, id, middle = ""):
   username = ""
-  #Adding first name
+  
   if len(first) <= 3:
     username += first.upper()
   else:
     username += first[:3].upper()
 
-  #Adding middle name
   username += middle
-
-  #Adding last name
   username += last[-3:].lower()
 
-  #Adding id
   id = str(id)
   username += "_" + id[-4:]
 
   return username
 
 first_name, middle_name, last_name, student_id= input ("First Name: "), input("Middle Name: "), input ("Last Name: "), int (input ("Student ID: "))
+
 
 print(username_generator(first_name, last_name, student_id, middle_name))
